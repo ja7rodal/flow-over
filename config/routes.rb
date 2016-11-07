15a2e4578a3d9f989bfc56f786c322256a8054dd
Jasson Rdz <ja7rodal@gmail.com>
2016-11-07 16:41:05 -0500

    add function to answer

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :questions do
       resources :comments, only: [:create]
       resource :voqte, only: [:create, :destroy]
+      resources :answers, only: [:create]
     end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
